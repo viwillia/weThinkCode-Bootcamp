3639566adf6def61ec82e9e69b9531901a5cac7a
Vicario Williams <viwillia@c2r4s4.wethinkcode.co.za>
2020-06-20 14:55:55 +0200

    ex 09

--- day03/ex09/ft_sort_integer_table.c
+++ day03/ex09/ft_sort_integer_table.c
@@ -9,3 +9,28 @@
 /*   Updated: 2020/06/19 13:01:18 by viwillia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+int	*ft_sort_integer_table(int *tab, int size)
+{
+	int	count;
+	int	i;
+	int	tmp;
+
+	count = 0;
+	while (count < size)
+	{
+		i = 0;
+		while (i < (size - 1))
+		{
+			if (tab[i] > tab[i + 1])
+			{
+				tmp = tab[i];
+				tab[i] = tab[i + 1];
+				tab[i + 1] = tmp;
+			}
+			i++;
+		}
+		count++;
+	}
+	return (tab);
+}
