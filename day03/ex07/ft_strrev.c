a6b110f3d1bc9cc31e8544b526b290b46b69f31e
Vicario Williams <viwillia@c2r4s4.wethinkcode.co.za>
2020-06-20 14:46:17 +0200

    ex 07

--- day03/ex07/ft_strrev.c
+++ day03/ex07/ft_strrev.c
@@ -9,3 +9,27 @@
 /*   Updated: 2020/06/19 13:01:00 by viwillia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+char *ft_strrev(char *str)
+{
+    int length;
+    int n;
+    char *new_str;
+
+    n = 0;
+    while(str[n] != '\0')
+    {
+        length++;
+        n++;
+    }
+
+    new_str = malloc(length * sizeof(char));
+    n = 0;
+    while (n < length)
+    {
+        new_str[length - n] = str[n];
+        n++;
+    }
+
+    return new_str;
+}
