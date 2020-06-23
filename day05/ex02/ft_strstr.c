3347193bfb8c1d4c9ef1686a748f13425105af89
root <root@NB7D287H2.metmom.mmih.biz>
2020-06-23 21:58:53 +0200

    ex02

--- day05/ex02/ft_strstr.c
+++ day05/ex02/ft_strstr.c
@@ -1,57 +1,26 @@
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-
-int ft_putchar(char c)
-{
-    write(1, &c, 1);
-    return 0;
-}
-
-int ft_putstr(char* str)
-{
-    int i;
-
-    i = -1;
-    while(str[++i] != '\0')
-    {
-        ft_putchar(str[i]);
-    }
-    return 0;
-}
-
 char *ft_strstr(char *str, char *to_find)
 {
+    char *substr;
     int length_substring;
     int n;
     int m;
-    int i;
-    char *substr;
 
     length_substring = -1;
-    while(to_find[++length_substring] != '\0');  
-
-    n = -1;
-    m = -1;
-    i = -1;
+    while(to_find[++length_substring] != '\0');
 
     substr = malloc(length_substring * sizeof(char));
-
-    while(str[++n] != '\0')
+    n = 0;
+    m = 0;
+    while(str[m++] != '\0')
     {
-        while(to_find[++m] != '\0')
+        if(str[m] == to_find[n])
         {
-            if(str[n] == to_find[m])
-            {
-                substr[++i] = str[n];
-            }
+            substr[n++] = str[m];
         }
     }
-    return substr;
-}
 
-int main(int argc, char **argv)
-{
-    ft_putstr(ft_strstr(argv[1], argv[2]));
-    ft_putchar('\n');
+    if(to_find[length_substring - 1] != substr[length_substring - 1])
+        return NULL;
+
+    return substr;
 }
