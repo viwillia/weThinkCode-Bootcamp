8b43ab637940a5fdff4da890fe1a186e737e4e8f
Vicario Williams <viwillia@c2r4s4.wethinkcode.co.za>
2020-06-25 15:50:06 +0200

    Updated ex4

--- day06/ex04/ft_sort_params.c
+++ day06/ex04/ft_sort_params.c
@@ -1,48 +1,60 @@
 #include <unistd.h>
 
-void putchar(char c)
+void ft_putchar(char c)
 {
     write(1, &c, 1);
 }
 
-void putstr(char *str)
+void ft_putstr(char *str)
 {
     int i;
 
     i = 0;
     while (str[i] != '\0')
     {
-        putchar(str[i]);
+        ft_putchar(str[i]);
         i++;
     }
 }
 
-int strcmp(char* str1, char * str2)
+int ft_strcmp(char *s1, char *s2)
 {
     int i;
-
     i = 0;
-    while(str1[i] != '\0')
+    while (s1[i] != '\0' && s2[i] != '\0')
     {
-        if     (str1[i] > str2[i]) return  1;
-        else if(str1[i] < str2[i]) return -1;
+        if (s1[i] > s2[i]) return 1;
+        else if (s1[i] < s2[i]) return -1;
         i++;
     }
-    if     (str1[i] != '\0' && str2[i] == '\0') return  1;
-    else if(str1[i] == '\0' && str2[i] != '\0') return -1;
-    
-    return 0;
+    if (s1[i] == '\0' && s2[i] != '\0') return -1;
+    else if (s1[i] != '\0' && s2[i] == '\0') return 1;
+    else return 0;
 }
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
     int i;
+    char* temp;
+
+    i = 1;
+    while (i < argc - 1)
+    {
+        if(ft_strcmp(argv[i], argv[i + 1]) > 0)
+        {
+            temp = argv[i];
+            argv[i] = argv[i + 1];
+            argv[i + 1] = temp;
+            i = 1;
+        } else 
+            i++;
+    }
 
     i = 1;
     while (i < argc)
     {
-        putstr((char*) argv[i]);
-        putchar('\n');
+        ft_putstr(argv[i]);
+        ft_putchar('\n');
         i++;
     }
     return 0;
