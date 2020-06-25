35bf455e019fedffa9f708880b4058fdbebe7337
Vicario Williams <viwillia@c2r4s4.wethinkcode.co.za>
2020-06-25 10:49:46 +0200

    ex 2 - 4

---
+++ day06/ex03/ft_rev_params.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+
+void putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void putstr(char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i] != '\0')
+    {
+        putchar(str[i]);
+        i++;
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    int i;
+
+    i = argc - 1;
+    while (i > 0)
+    {
+        putstr((char*) argv[i]);
+        putchar('\n');
+        i--;
+    }
+    return 0;
+}
---
+++ day06/ex04/ft_sort_params.c
@@ -0,0 +1,49 @@
+#include <unistd.h>
+
+void putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void putstr(char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i] != '\0')
+    {
+        putchar(str[i]);
+        i++;
+    }
+}
+
+int strcmp(char* str1, char * str2)
+{
+    int i;
+
+    i = 0;
+    while(str1[i] != '\0')
+    {
+        if     (str1[i] > str2[i]) return  1;
+        else if(str1[i] < str2[i]) return -1;
+        i++;
+    }
+    if     (str1[i] == '\0' && str2[i] != '\0') return -1;
+    else if(str1[i] != '\0' && str2[i] == '\0') return  1;
+    
+    return 0;
+}
+
+int main(int argc, char const *argv[])
+{
+    int i;
+
+    i = 1;
+    while (i < argc)
+    {
+        putstr((char*) argv[i]);
+        putchar('\n');
+        i++;
+    }
+    return 0;
+}
