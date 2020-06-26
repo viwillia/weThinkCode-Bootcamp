f5943c0c3422b4aca440e34b7808e0452338be49
root <root@NB7D287H2.metmom.mmih.biz>
2020-06-26 20:30:35 +0200

    Conformed to prototype

--- day07/ex04/ft_split_whitespaces.c
+++ day07/ex04/ft_split_whitespaces.c
@@ -46,7 +46,7 @@ int	ft_word_count(char *string)
 	return ++count;
 }
 
-char **ft_split_whitespaces(char *string)
+char **ft_split_whitespaces(char *str)
 {
     char **table;
 	int index;
@@ -56,24 +56,24 @@ char **ft_split_whitespaces(char *string)
 
 	index = 0;
 	j = 0;
-	table = malloc(ft_word_count(string));
-	while (string[j])
+	table = malloc(ft_word_count(str));
+	while (str[j])
 	{
 		k = 1;
-		while (is_terminating_character(string[j]))
+		while (is_terminating_character(str[j]))
             j++;
 
-    	length = ft_word_length(j, string);
+    	length = ft_word_length(j, str);
 		table[index] = malloc(++length * sizeof(char*));
-		while (!is_terminating_character(string[j]))
+		while (!is_terminating_character(str[j]))
 		{
-			table[index][k - 1] = string[j++];
+			table[index][k - 1] = str[j++];
 			k++;
 		}
 
 		table[index++][k - 1] = '\0';
 	}
-    
+
 	table[index] = 0;
 	return table;
 }
