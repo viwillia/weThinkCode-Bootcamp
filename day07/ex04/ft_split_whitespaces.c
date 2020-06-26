bb203ae8ef441d0657edc36402bbfcd0cbb2ab2c
root <root@NB7D287H2.metmom.mmih.biz>
2020-06-26 20:27:11 +0200

    Refactorted and recieved guidence from @trhoda

--- day07/ex04/ft_split_whitespaces.c
+++ day07/ex04/ft_split_whitespaces.c
@@ -12,48 +12,68 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-char **ft_split_whitespaces(char *str)
+int is_terminating_character(char c)
 {
-    int i;
-    int j;
-    int n;
-    int length;
-    char ** split;
-
-    i = 0;
-    length = 0;
-    while(str[i] != '\0')
-    {
-        if(str[i] == ' ' || str[i] != '\t' || str[i] != '\n')
-            i++;
-            continue;
-        i++;
-        length++;
-    }
-
-    split = malloc(length * sizeof(char));
-    i = 0;
-    j = -1;
-    n = 0;
-
-    while(i < length)
-    {
-        if(str[i] == ' ' || str[i] != '\t' || str[i] != '\n')
-        {
-            n = 0;
-            j++;
-        }
-        split[j][n] = str[i];
-        n++;
-        i++;
-    }
-
-    return split;
+    return (
+        c == '\n' ||
+        c == '\t' ||
+        c == ' '  ||
+        c == '\0'
+    );
 }
 
-int main()
+int	ft_word_length(int index, char *string)
 {
-    char **nice = ft_split_whitespaces("My\tname is\tvicario williams");
-    printf("%s\n", nice[3]);
-    return 0;
+	int	i;
+
+	i = index;
+	while (!is_terminating_character(string[index]))
+        index++;
+	return index - i;
+}
+
+int	ft_word_count(char *string)
+{
+	int count;
+	int index;
+
+	index = 0;
+	count = 0;
+	while (string[index])
+	{
+        if (is_terminating_character(string[index]))
+            count++;
+		index++;
+	}
+	return ++count;
+}
+
+char **ft_split_whitespaces(char *string)
+{
+    char **table;
+	int index;
+	int	j;
+	int	k;
+  	int length;
+
+	index = 0;
+	j = 0;
+	table = malloc(ft_word_count(string));
+	while (string[j])
+	{
+		k = 1;
+		while (is_terminating_character(string[j]))
+            j++;
+
+    	length = ft_word_length(j, string);
+		table[index] = malloc(++length * sizeof(char*));
+		while (!is_terminating_character(string[j]))
+		{
+			table[index][k - 1] = string[j++];
+			k++;
+		}
+		table[index++][k - 1] = '\0';
+	}
+	table[index] = 0;
+	return table;
 }
