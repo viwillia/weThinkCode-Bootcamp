5ea7ab33dd57da3ca744168045a1211f0e46e400
root <root@NB7D287H2.metmom.mmih.biz>
2020-06-26 19:22:59 +0200

    ex03

--- day07/ex03/ft_concat_params.c
+++ day07/ex03/ft_concat_params.c
@@ -13,5 +13,33 @@
 
 char *ft_concat_params(int argc, char **argv)
 {
-    
+    int n;
+    int m;
+    int length;
+    char* concat;
+
+    n = 0;
+    length = 0;
+
+    while(++n < argc)
+    {
+        m = 0;
+        while(argv[n][m++] != '\0')
+            length++;
+        length++;
+    }
+
+    concat = malloc(length * sizeof(char));
+
+    n = 0;
+    length = 0;
+    while(++n < argc)
+    {
+        m = 0;
+        while(argv[n][m] != '\0')
+            concat[length++] = argv[n][m++];
+        concat[length++] = '\n';
+    }
+    concat[length - 1] = '\0';
+    return concat;
 }
