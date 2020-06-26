5d849488a8eed5e6b2f0aff0f627406f9c02fc48
root <root@NB7D287H2.metmom.mmih.biz>
2020-06-26 19:43:07 +0200

    ex04 not working :(

--- day07/ex04/ft_split_whitespaces.c
+++ day07/ex04/ft_split_whitespaces.c
@@ -9,9 +9,51 @@
 /*   Updated: 2020/06/26 14:55:08 by viwillia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+#include <stdlib.h>
+#include <stdio.h>
 
 char **ft_split_whitespaces(char *str)
 {
-    
+    int i;
+    int j;
+    int n;
+    int length;
+    char ** split;
+
+    i = 0;
+    length = 0;
+    while(str[i] != '\0')
+    {
+        if(str[i] == ' ' || str[i] != '\t' || str[i] != '\n')
+            i++;
+            continue;
+        i++;
+        length++;
+    }
+
+    split = malloc(length * sizeof(char));
+    i = 0;
+    j = -1;
+    n = 0;
+
+    while(i < length)
+    {
+        if(str[i] == ' ' || str[i] != '\t' || str[i] != '\n')
+        {
+            n = 0;
+            j++;
+        }
+        split[j][n] = str[i];
+        n++;
+        i++;
+    }
+
+    return split;
+}
+
+int main()
+{
+    char **nice = ft_split_whitespaces("My\tname is\tvicario williams");
+    printf("%s\n", nice[3]);
+    return 0;
 }
